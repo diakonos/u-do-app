32c64a4a0894be7b0b88992035c088479259057b
Justin Ling <jlinged@gmail.com>
2025-04-28 17:43:39 +0800

    dont render html head on native

--- frontend/components/HTMLTitle.tsx
+++ frontend/components/HTMLTitle.tsx
@@ -1,7 +1,9 @@
 import Head from "expo-router/head";
 import { PropsWithChildren } from "react";
+import { Platform } from "react-native";
 
 export function HTMLTitle({ children }: PropsWithChildren) {
+  if (Platform.OS !== 'web') return null;
   return (
     <Head>
       <title>{children} - U Do</title>
