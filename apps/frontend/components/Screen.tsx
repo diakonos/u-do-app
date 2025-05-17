b9dcc0a711c2a24f262cc9f7940df7988ca19e4f
Justin Ling <jlinged@gmail.com>
2025-05-17 10:30:25 +0800

    remove safe area view from screen component

--- apps/frontend/components/Screen.tsx
+++ apps/frontend/components/Screen.tsx
@@ -1,8 +1,6 @@
-import { useTheme } from '@/lib/theme';
 import { PropsWithChildren } from 'react';
-import { ViewStyle } from 'react-native';
-import { StyleSheet } from 'react-native';
-import { SafeAreaView } from 'react-native';
+import { StyleSheet, View, ViewStyle } from 'react-native';
+import { useTheme } from '@/lib/theme';
 
 type ScreenProps = PropsWithChildren & {
   style?: ViewStyle | ViewStyle[];
@@ -12,12 +10,9 @@ export default function Screen(props: ScreenProps) {
   const { children, style, ...rest } = props;
   const theme = useTheme();
   return (
-    <SafeAreaView
-      style={[styles.container, { backgroundColor: theme.background }, style]}
-      {...rest}
-    >
+    <View style={[styles.container, { backgroundColor: theme.background }, style]} {...rest}>
       {children}
-    </SafeAreaView>
+    </View>
   );
 }
 
