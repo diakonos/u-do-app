50f36a807772c135665efe92b236df2caed9fbee
Justin Ling <jlinged@gmail.com>
2025-08-13 22:50:56 +0800

    fix frontend build

--- apps/frontend/metro.config.js
+++ apps/frontend/metro.config.js
@@ -2,6 +2,7 @@
 const { getDefaultConfig } = require('expo/metro-config');
 const { withSentryConfig } = require('@sentry/react-native/metro');
 const { withNativeWind } = require('nativewind/metro');
+const path = require('path');
 
 module.exports = (() => {
   const config = getDefaultConfig(__dirname);
@@ -12,12 +13,21 @@ module.exports = (() => {
     ...transformer,
     babelTransformerPath: require.resolve('react-native-svg-transformer/expo'),
   };
+
   config.resolver = {
     ...resolver,
     assetExts: resolver.assetExts.filter(ext => ext !== 'svg'),
     sourceExts: [...resolver.sourceExts, 'svg'],
     unstable_enablePackageExports: true,
+    // Fix for monorepo setup - look for node_modules in current directory first
+    nodeModulesPaths: [
+      path.resolve(__dirname, 'node_modules'),
+      path.resolve(__dirname, '../../node_modules'),
+    ],
   };
+
+  // Set watchFolders to include monorepo root for proper file watching
+  config.watchFolders = [path.resolve(__dirname, '../../')];
 
   return withSentryConfig(withNativeWind(config, { input: './assets/global.css' }));
 })();
